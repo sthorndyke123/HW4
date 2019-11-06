6767297952ec68b5d778c9a63888f0062cc9db8b
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-11-06 15:22:34 -0600

    starting of WTF froms

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StingField
+from wtforms.validators import DataRequired
 
 app =Flask(__name__)
+app.config['SECRET_KEY'] = 'SuperSecretKey'
+
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', pageTitle='Horror Films')
 
 
 if __name__ == '__main__':
