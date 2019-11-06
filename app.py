53048b9940e69c1e49089a2a9b8922106b454d7a
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-11-06 15:03:08 -0600

    modified application to use basic jinja template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app =Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
 
 if __name__ == '__main__':
