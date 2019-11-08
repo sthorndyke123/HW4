f82e6fcdfc1e4d94c4d5017800ae01848f762fc6
Soraya Thorndyke <sthorndyke325@gmail.com>
2019-11-08 15:33:13 -0600

    finalized and fixed adding a book

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = conn
 db = SQLAlchemy(app)
 
 
-class sthorndyke_Books(db.Model):
+class sthorndyke_books(db.Model):
     id = db. Column(db.Integer, primary_key=True)
     Title_of_Book = db.Column(db.String(255))
     Authors_Last_Name = db.Column(db.String(255))
@@ -29,20 +29,25 @@ class BookForm(FlaskForm):
 
 @app.route('/')
 def index():
-     all_books = sthorndyke_Books.query.all()
+    all_books = sthorndyke_books.query.all()
     return render_template('index.html', books=all_books, pageTitle='Favorite Books')
 
 
 @app.route('/add_book', methods=['GET','POST'])
 def add_book():
     form = BookForm()
+    print("before validate")
     if form.validate_on_submit():
-        book = sthorndyke_Books(Title_of_Book=form.Title_of_Book.data, Authors_Last_Name=form.Authors_Last_Name.data)
+        print("before object")
+        book = sthorndyke_books(Title_of_Book=form.Title_of_Book.data, Authors_Last_Name=form.Authors_Last_Name.data)
+        print(book)
         db.session.add(book)
         db.session.commit()
-        return "<h2> My Favorite book is {0} {1}".format(form.Title_of_Book.data, form.Authors_Last_Name.data)
+        return redirect('/')
 
     return render_template('add_book.html', form=form, pageTitle='Add a New Book')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
